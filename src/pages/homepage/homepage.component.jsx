218f06b994ec52933d42e464429984e889dbb4cf
eladiad <eladiadsadiku@gmail.com>
2022-06-26 17:48:47 +0200

    add new components, created directory with childrents menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../copponents/directory/directory.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
