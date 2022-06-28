7b50ff9cb9d98e78335dbd8437e97dcbb5d1d696
eladiad <eladiadsadiku@gmail.com>
2022-06-28 09:57:47 +0200

    fix routers with the new react version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
 
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
 
-import './App.css';
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
